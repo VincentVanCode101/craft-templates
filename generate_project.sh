51847431a7a7e0f5dbff287ec8a3a5940fb0dc48
Christoph Trost <christoph.tech101@gmail.com>
2025-02-06 07:19:00 +0100

    Do 6. Feb 07:19:00 CET 2025

--- generate_project.sh
+++ generate_project.sh
@@ -7,29 +7,22 @@ if [ -z "$1" ]; then
 fi
 
 PROJECT_NAME="$1"
-GROUP_ID="com.main"
-DOCKER_IMAGE_NAME="maven-project-generator"
 DOCKERFILE="build.Dockerfile"
 
-# Get user and group IDs
 U_ID=$(id -u)
 G_ID=$(id -g)
 
-# Build the Docker image
 docker build \
-    -f "$DOCKERFILE" \
-    --build-arg UID="$U_ID" \
-    --build-arg G_ID="$G_ID" \
-    --build-arg GROUP_ID="$GROUP_ID" \
-    --build-arg ARTIFACT_ID="$PROJECT_NAME" \
-    -t "$DOCKER_IMAGE_NAME" .
+    -f $DOCKERFILE \
+    --build-arg UID=$U_ID \
+    --build-arg GID=$GID \
+    --build-arg PROJECT_NAME=$PROJECT_NAME \
+    -t $DOCKER_IMAGE_NAME .
 
-# Run the container and copy the generated project files to workspace
 docker run --rm \
     -v "$(pwd):/workspace" \
-    "$DOCKER_IMAGE_NAME" \
-    /bin/bash -c "cp -p -r /build-space/* /workspace"
+    $DOCKER_IMAGE_NAME \
+    /bin/sh -c "cp -p -r /build-space/* /workspace"
 
-# Remove `build.Dockerfile` after the build
 echo "Cleaning up: Removing build.Dockerfile..."
 rm -f "$DOCKERFILE"
